54ba35f329cb419eb8665a6977adaf7cf3a563f0
ahmedooo1 <aahmadooo997@gmail.com>
2023-06-30 15:47:01 +0200

    modal nav profil  updated

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,19 +8,38 @@
           </NuxtLink>
         </div>
 
-        <button @click="toggleMenu" class="md:hidden">
+        <button @click="toggleMenu" class="absolute text-white right-6 md:hidden">
           <!-- Icône du menu burger -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
         </button>
 
-        <div :class="{ 'hidden': !isMenuVisible, 'block': isMenuVisible }" class="flex items-center justify-between md:flex md:ml-6">
-          <NuxtLink to="/guidesList" class="flex px-4 py-2 mx-8 text-white rounded-full hover:bg-white hover:text-gray-800 "><svg
+        <div
+          :class="{ hidden: !isMenuVisible, block: isMenuVisible }"
+          class="flex items-center justify-between md:flex md:ml-6"
+        >
+          <NuxtLink
+            to="/guidesList"
+            class="flex px-4 py-2 mx-8 text-white rounded-full hover:bg-white hover:text-gray-800"
+            ><svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
               class="w-6 h-6 mx-2"
-
             >
               <path
                 stroke-linecap="round"
@@ -28,7 +47,7 @@
                 d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
               />
             </svg>
-             Guides
+            Guides
           </NuxtLink>
           <button
             v-if="!isLoggedIn"
@@ -50,28 +69,29 @@
                 d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
               />
             </svg>
-           Connexion
+            Connexion
           </button>
 
-        
-            <NuxtLink to="/profil" v-if="isLoggedIn" class="flex items-center px-4 py-2 mx-8 text-white rounded-full hover:bg-white hover:text-gray-800 "
-              ><svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-8 h-8 mx-2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-              Profil
-            </NuxtLink>
-        
+          <NuxtLink
+            to="/profil"
+            v-if="isLoggedIn"
+            class="flex items-center px-4 py-2 mx-8 text-white rounded-full hover:bg-white hover:text-gray-800"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-8 h-8 mx-2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+            Profil
+          </NuxtLink>
 
           <button
             v-if="isLoggedIn"
@@ -116,7 +136,7 @@ export default {
       isModalOpen: false,
       isOpen: false,
       categories: [],
-          isMenuVisible: false,
+      isMenuVisible: false,
       isModalOpen: false,
       isLoggedIn: false,
     };
@@ -126,7 +146,7 @@ export default {
     ...mapGetters(["isLoggedIn"]),
   },
   methods: {
-       toggleMenu() {
+    toggleMenu() {
       this.isMenuVisible = !this.isMenuVisible;
     },
     openModal() {
