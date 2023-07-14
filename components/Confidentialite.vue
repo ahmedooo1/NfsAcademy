327a5df3a7e457d17f27a5021d46c18970e8e376
ahmedooo1 <aahmadooo997@gmail.com>
2023-07-14 03:02:49 +0200

    Update Confidentialite.vue

--- components/Confidentialite.vue
+++ components/Confidentialite.vue
@@ -7,39 +7,33 @@
     >
       <h2 class="mb-2 text-3xl font-bold">Politique de confidentialité</h2>
       <p class="mb-4">
-        Votre confidentialité est importante pour nous. Nous collectons et
-        utilisons vos informations personnelles seulement pour permettre la
-        création de votre compte utilisateur, vous fournir des services sur
-        notre site et améliorer votre expérience utilisateur.
+        Votre confidentialité est importante pour nous. Nous collectons et utilisons vos
+        informations personnelles seulement pour permettre la création de votre compte
+        utilisateur, vous fournir des services sur notre site et améliorer votre
+        expérience utilisateur.
+      </p>
+      <p class="mb-4">Nous ne vendons pas vos informations personnelles à des tiers.</p>
+      <p class="mb-4">
+        Si vous souhaitez accéder, modifier ou supprimer vos informations personnelles, ou
+        si vous avez des questions concernant notre politique de confidentialité, veuillez
+        nous contacter à l'adresse e-mail suivante : nfs-academy@outlook.com.
       </p>
       <p class="mb-4">
-        Nous ne vendons pas vos informations personnelles à des tiers. Nous
-        utilisons des cookies pour améliorer l'expérience utilisateur et pour
-        surveiller l'activité du site. Vous pouvez modifier les paramètres de
-        votre navigateur pour désactiver les cookies si vous le souhaitez.
-      </p>
-      <p class="mb-4">
-        Si vous souhaitez accéder, modifier ou supprimer vos informations
-        personnelles, ou si vous avez des questions concernant notre politique
-        de confidentialité, veuillez nous contacter à l'adresse e-mail suivante
-        : nfs-academy@outlook.com.
-      </p>
-      <p class="mb-4">
-        Nous nous réservons le droit de modifier cette politique de
-        confidentialité à tout moment. Les utilisateurs seront informés des
-        changements par le biais d'une notification sur le site ou par e-mail.
+        Nous nous réservons le droit de modifier cette politique de confidentialité à tout
+        moment. Les utilisateurs seront informés des changements par le biais d'une
+        notification sur le site ou par e-mail.
       </p>
       <h1 class="font-bold">Gestion des cookies 🍪 :</h1>
       <p class="mb-4">
-        Nous utilisons des cookies pour améliorer l'expérience utilisateur et
-        pour surveiller l'activité du site. Si vous ne souhaitez pas autoriser
-        l'utilisation de cookies, vous pouvez modifier les paramètres de votre
-        navigateur pour désactiver les cookies.
+        Nous utilisons des cookies pour améliorer l'expérience utilisateur et pour
+        surveiller l'activité du site. Si vous ne souhaitez pas autoriser l'utilisation de
+        cookies, vous pouvez modifier les paramètres de votre navigateur pour désactiver
+        les cookies.
       </p>
       <p class="mb-4">
-        Les cookies que nous utilisons nous permettent d'analyser le trafic sur
-        notre site, de personnaliser le contenu et les publicités, et de fournir
-        des fonctionnalités de médias sociaux.
+        Les cookies que nous utilisons nous permettent d'analyser le trafic sur notre
+        site, de personnaliser le contenu et les publicités, et de fournir des
+        fonctionnalités de médias sociaux.
       </p>
     </div>
     <img
