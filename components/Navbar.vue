b6eed5f04064ce8f4ce68821404af2905958af0d
ahmedooo1 <aahmadooo997@gmail.com>
2023-07-16 18:25:35 +0200

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,36 +1,38 @@
 <template>
   <div>
-    <nav class="relative w-full py-2 mx-auto bg-blue-700 lg:px-6 md:flex md:justify-between md:items-center">
+    <nav
+      class="relative w-full py-2 mx-auto bg-blue-700 lg:px-6 md:flex md:justify-between md:items-center"
+    >
       <div class="container px-6 py-6 mx-auto md:flex md:justify-between md:items-center">
-        <div class="flex items-center justify-between w-full lg:flex-wrap ">
-            <button
-              @click="toggleDropdown"
-              class="flex w-full text-white lg:hidden focus:outline-none"
+        <div class="flex items-center justify-between w-full lg:flex-wrap">
+          <button
+            @click="toggleDropdown"
+            class="flex w-full text-white lg:hidden focus:outline-none"
+          >
+            <svg
+              class="w-6 h-6 fill-current hover:text-gray-300"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                class="w-6 h-6 fill-current hover:text-gray-300"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  v-if="!isOpen"
-                  d="M4 6H20M4 12H20M4 18H20"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  v-else
-                  d="M6 18L18 6M6 6l12 12"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
-              <!-- <NuxtLink to="/" class="float-right mb-3 text-white lg:hidden">
+              <path
+                v-if="!isOpen"
+                d="M4 6H20M4 12H20M4 18H20"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                v-else
+                d="M6 18L18 6M6 6l12 12"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+          <!-- <NuxtLink to="/" class="float-right mb-3 text-white lg:hidden">
               <img
                 src="../assets/images/logoWhite.png"
                 alt="Logo"
@@ -38,53 +40,48 @@
               />
           
             </NuxtLink> -->
-            <NuxtLink to="/" class="block float-right text-white lg:flex">
-              <img
-                src="../assets/images/logoWhite.png"
-                alt="Logo"
-                class="w-28"
-              />
-              <div
-                class="items-center hidden px-4 py-2 mx-8 text-white rounded-full fill-current lg:flex hover:bg-white hover:text-gray-800"
+          <NuxtLink to="/" class="block float-right text-white lg:flex">
+            <img src="../assets/images/logoWhite.png" alt="Logo" class="w-28" />
+            <div
+              class="items-center hidden px-4 py-2 mx-8 text-white rounded-full fill-current lg:flex hover:bg-white hover:text-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 8.467 8.467"
+                id="home"
+                class="mx-2 hover:text-gray-800"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 8.467 8.467"
-                  id="home"
-                  class="mx-2 hover:text-gray-800"
-                >
-                  <path
-                    d="m 4.2189035,0.79415377 a 0.26391311,0.26459421 0 0 0 -0.1443101,0.052707 L 0.91104361,3.2279704 A 0.26391311,0.26459421 0 1 0 1.2285335,3.6496251 L 4.2323036,1.387881 7.2381335,3.6496251 a 0.26391311,0.26459421 0 1 0 0.31749,-0.4216547 L 4.3920736,0.84686077 a 0.26391311,0.26459421 0 0 0 -0.1731701,-0.052707 z m 0,1.17763653 A 0.26391311,0.26459421 0 0 0 4.0745934,2.0244973 L 1.4388135,4.0149559 a 0.26391311,0.26459421 0 0 0 -0.10514,0.2108274 v 3.1815302 a 0.26391311,0.26459421 0 0 0 0.26337,0.2656009 H 3.4416736 A 0.26391311,0.26459421 0 0 0 3.7065934,7.4073135 l -0.006,-1.851457 h 1.05967 v 1.851457 a 0.26391311,0.26459421 0 0 0 0.26337,0.2656009 h 1.84669 A 0.26391311,0.26459421 0 0 0 7.1331826,7.4073135 V 4.2257833 A 0.26391311,0.26459421 0 0 0 7.0280435,4.0149559 l -2.63578,-1.9904587 a 0.26391311,0.26459421 0 0 0 -0.17317,-0.052707 z m 0.013401,0.5957942 2.3729199,1.7889326 V 7.1437795 H 5.2883636 V 5.2902553 A 0.26391311,0.26459421 0 0 0 5.0234435,5.0267207 h -1.58589 a 0.26391311,0.26459421 0 0 0 -0.26492,0.2656012 l 0.006,1.8514576 h -1.31667 V 4.3565171 Z"
-                    
-                    font-family="sans-serif"
-                    font-weight="400"
-                    overflow="visible"
-                    style="
-                      line-height: normal;
-                      font-variant-ligatures: normal;
-                      font-variant-position: normal;
-                      font-variant-caps: normal;
-                      font-variant-numeric: normal;
-                      font-variant-alternates: normal;
-                      font-feature-settings: normal;
-                      text-indent: 0;
-                      text-align: start;
-                      text-decoration-line: none;
-                      text-decoration-style: solid;
-                      text-decoration-color: #000;
-                      text-transform: none;
-                      text-orientation: mixed;
-                      shape-padding: 0;
-                      isolation: auto;
-                      mix-blend-mode: normal;
-                    "
-                  ></path>
-                </svg>
-                Home
-              </div>
-            </NuxtLink>
+                <path
+                  d="m 4.2189035,0.79415377 a 0.26391311,0.26459421 0 0 0 -0.1443101,0.052707 L 0.91104361,3.2279704 A 0.26391311,0.26459421 0 1 0 1.2285335,3.6496251 L 4.2323036,1.387881 7.2381335,3.6496251 a 0.26391311,0.26459421 0 1 0 0.31749,-0.4216547 L 4.3920736,0.84686077 a 0.26391311,0.26459421 0 0 0 -0.1731701,-0.052707 z m 0,1.17763653 A 0.26391311,0.26459421 0 0 0 4.0745934,2.0244973 L 1.4388135,4.0149559 a 0.26391311,0.26459421 0 0 0 -0.10514,0.2108274 v 3.1815302 a 0.26391311,0.26459421 0 0 0 0.26337,0.2656009 H 3.4416736 A 0.26391311,0.26459421 0 0 0 3.7065934,7.4073135 l -0.006,-1.851457 h 1.05967 v 1.851457 a 0.26391311,0.26459421 0 0 0 0.26337,0.2656009 h 1.84669 A 0.26391311,0.26459421 0 0 0 7.1331826,7.4073135 V 4.2257833 A 0.26391311,0.26459421 0 0 0 7.0280435,4.0149559 l -2.63578,-1.9904587 a 0.26391311,0.26459421 0 0 0 -0.17317,-0.052707 z m 0.013401,0.5957942 2.3729199,1.7889326 V 7.1437795 H 5.2883636 V 5.2902553 A 0.26391311,0.26459421 0 0 0 5.0234435,5.0267207 h -1.58589 a 0.26391311,0.26459421 0 0 0 -0.26492,0.2656012 l 0.006,1.8514576 h -1.31667 V 4.3565171 Z"
+                  font-family="sans-serif"
+                  font-weight="400"
+                  overflow="visible"
+                  style="
+                    line-height: normal;
+                    font-variant-ligatures: normal;
+                    font-variant-position: normal;
+                    font-variant-caps: normal;
+                    font-variant-numeric: normal;
+                    font-variant-alternates: normal;
+                    font-feature-settings: normal;
+                    text-indent: 0;
+                    text-align: start;
+                    text-decoration-line: none;
+                    text-decoration-style: solid;
+                    text-decoration-color: #000;
+                    text-transform: none;
+                    text-orientation: mixed;
+                    shape-padding: 0;
+                    isolation: auto;
+                    mix-blend-mode: normal;
+                  "
+                ></path>
+              </svg>
+              Home
+            </div>
+          </NuxtLink>
 
           <div
             v-show="isOpen"
@@ -109,10 +106,7 @@
               </svg>
             </button> -->
 
-            <NuxtLink
-              to="/"
-              class="w-full py-4 text-center text-white hover:bg-blue-600"
-            >
+            <NuxtLink to="/" class="w-full py-4 text-center text-white hover:bg-blue-600">
               Home
             </NuxtLink>
             <NuxtLink
@@ -121,10 +115,17 @@
             >
               Guides
             </NuxtLink>
+            <a
+              href="http://localhost/NfsAcademy_wp/boutique/"
+              class="w-full py-4 text-center text-white hover:bg-blue-600"
+              target="_blank"
+            >
+              Boutique
+            </a>
             <button
               v-if="!isLoggedIn"
               @click="openModal"
-              class="w-full py-4 text-center text-white rounded-b-lg hover:bg-blue-600 focus:outline-none "
+              class="w-full py-4 text-center text-white rounded-b-lg hover:bg-blue-600 focus:outline-none"
             >
               Connexion
             </button>
@@ -165,13 +166,35 @@
               </svg>
               Guides
             </NuxtLink>
+            <a
+              href="http://localhost/NfsAcademy_wp/boutique/"
+              target="_blank"
+              class="hidden px-4 py-2 text-white rounded-full lg:flex hover:bg-white hover:text-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
+                />
+              </svg>
+
+              Boutique
+            </a>
             <button
               v-if="!isLoggedIn"
               @click="openModal"
               class="hidden px-4 py-2 text-white rounded-full lg:flex hover:bg-white hover:text-gray-800"
             >
               <svg
-                class="w-6 h-6 mx-2 dark:text-white"
+                class="w-6 h-6 mx-2"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -234,9 +257,8 @@
           </div>
         </div>
       </div>
-
-      
-    </nav><Modal :isOpen="isModalOpen" @close="closeModal" />
+    </nav>
+    <Modal :isOpen="isModalOpen" @close="closeModal" />
   </div>
 </template>
 
@@ -291,15 +313,15 @@ export default {
 </script>
 
 <style>
- /* Add your styles or override tailwind classes here */
+/* Add your styles or override tailwind classes here */
 
- /* Hide the navigation menu by default on small screens */
- .lg\\:hidden {
-        display: none;
-    }
+/* Hide the navigation menu by default on small screens */
+.lg\\:hidden {
+  display: none;
+}
 
-  /* Show the navigation menu when the hamburger icon is clicked */
-  .open {
-      display: flex;
-    }
+/* Show the navigation menu when the hamburger icon is clicked */
+.open {
+  display: flex;
+}
 </style>
