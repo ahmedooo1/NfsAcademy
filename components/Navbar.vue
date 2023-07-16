6ce7188673c8d6a9861b0030cc43af6c15a8fd86
ahmedooo1 <aahmadooo997@gmail.com>
2023-07-16 18:33:24 +0200

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -177,7 +177,7 @@
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                class="w-6 h-6"
+                class="w-6 h-6 mx-2"
               >
                 <path
                   stroke-linecap="round"
